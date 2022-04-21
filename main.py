fbab2201329ee9212e0c1800c6bb35047711ba57
unknown <iluha7394@mail.ru>
2022-04-21 16:27:08 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#money_keeper
+
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+class Money_keeper(MDApp):
+    def build(self):
+        return MDLabel(text="Hello, Money Keeper", halign="center")
+
+
+Money_keeper().run()
