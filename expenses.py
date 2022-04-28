6eab111c1c6bd05a6917408259c780cc970e489e
Ilya-sick <ilyasmakhmutov@yandex.ru>
2022-04-28 23:30:02 +0400

    3_split and check users message

--- expenses.py
+++ expenses.py
@@ -1,22 +1,29 @@
 #money_keeper
 
 categories = ['еда']
-users_message = 'еда 250'
+#users_message = 'еда 250'
 
 
 def add_expense(users_message):
     parsed_message = parse_message(users_message)
-    find_categories = add_categories(parsed_message[0])
+    #find_categories = add_categories(parsed_message[0])
+
+    return print(parsed_message)
 
 
 
 def parse_message(users_message):
     parsed = users_message.split(' ')
-    return parsed
+    if (len(parsed)) == 2:
+        return parsed
+    else:
+        return f"Чувак, что-то пошло не так, попробуй снова...)"
 
-def add_categories(first_part_message):
-    print(first_part_message)
-    if first_part_message in categories:
-        print('ehf)'
+#def add_categories(first_part_message):
+#    print(first_part_message)
+#    if first_part_message in categories:
+#        print('ehf)'
 
-    return first_part_message
+#    return 
+
+#add_expense(users_message)
