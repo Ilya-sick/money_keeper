a6f2cfdae4fbd8bcf05f7b4e977597fabe37788c
Ilya-sick <ilyasmakhmutov@yandex.ru>
2022-05-30 18:51:20 +0400

    30052022_add new handler (categories)__add text to all handler

--- expenses.py
+++ expenses.py
@@ -21,23 +21,22 @@ categories = {
     'other' : ['другое']
 }
 
-
+# looking for category and send parsed message to db
 def add_expense(users_message):
     parsed_message = parse_message(users_message)
     find_categories = add_categories(parsed_message[0])
     send_exp_cat_in_db = db.add_expense_and_category_in_db(parsed_message[1], find_categories)
             
-
-    return f'{parsed_message[1]}, {find_categories}'
-
+    return f'Ваш расход - {parsed_message[1]}р. добавлен в категорию {find_categories}!'
 
 
 def parse_message(users_message):
     parsed = users_message.split(' ')
-    if (len(parsed)) == 2 and int(parsed[1])/int(parsed[1]) == 1 and int(parsed[1]) != 0:
+    if (len(parsed)) == 2 and int(parsed[1]) != 0:
         return parsed
     else:
-        return f"Чувак, что-то пошло не так, попробуй снова...)"
+        return f"Что-то пошло не так, попробуйте снова...)"
+
 
 def add_categories(first_part_message):
     for category, category_values in categories.items():
@@ -45,4 +44,10 @@ def add_categories(first_part_message):
             if category is not None:
                 return f'{category}'
             else:
-                return f"Брат, такой категории нет..."
+                return f"Такой категории нет..."
+
+
+def show_all_categories_to_user():
+    for cat_key in categories:
+        print (f"{categories[cat_key]} : {categories[cat_key]}")
+        return f"{categories[cat_key]}"
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#money_keeper
+# money_keeper
 
 from config import TOKEN
 import logging
@@ -16,7 +16,6 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
     """
@@ -24,19 +23,40 @@ async def send_welcome(message: types.Message):
     """
     await message.reply(
         "Добро пожаловать!\n"
-        "I'm MoneyKeeperBot!\n"
+        "I'm MoneyKeeperBot!😊\n"
         "Пример добавления расхода: 'такси 50'\n"
+        "Посмотреть все категории: '/categories'\n"
         )
 
+@dp.message_handler(commands=['categories'])
+async def show_all_categories(message: types.Message):
+    """
+    This handler will be called when user sends `/categories'
+    """
+    for cat_key in expenses.categories:
+        show_all_categories = f"{expenses.categories[cat_key]}"
+        await message.answer(
+            f"{show_all_categories}\n"
+            )
+    await message.answer(
+        "Пример добавления расхода: 'такси 50'\n"
+        )
+    
 
+
+# handling users message
 @dp.message_handler()
 async def add_expense(message: types.Message):
     try:
         expense = expenses.add_expense(message.text)
-        t = f'Мы на верном пути, {expense}'
-        await message.answer(t)
+        text_answer = f'Готово! {expense}'
+        await message.answer(text_answer)
     except:
-        pass
+        await message.reply(
+            "Что-то пошло не так, попробуйте снова😔\n"
+            "Пример добавления расхода: 'такси 50'\n"
+            "Посмотреть все категории: '/categories'\n"
+            )
     
 
         
